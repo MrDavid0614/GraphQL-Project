0eeba4407b726d72d208f4d24000512914bafe32
A19B98585 educacion <A19B98585@educacion.edu.do>
2021-02-06 18:38:55 -1200

    Add test files

---
+++ backend/CRUD/django_graphql_cars/cars/tests/test_models.py
@@ -0,0 +1,4 @@
+class TestCarModel(TestCase):
+
+    def test_car_model_doesnt_receive_all_attributes(self):
+        pass
---
+++ backend/CRUD/django_graphql_cars/cars/tests/test_schema.py
@@ -0,0 +1,4 @@
+class TestSchema(TestCase):
+
+    def test_car_year_not_receive_a_valid_year(self):
+        pass
